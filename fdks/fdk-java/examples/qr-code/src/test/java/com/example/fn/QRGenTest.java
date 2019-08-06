c5cde451aeaeac0c6c03984649c0ad6d679027d4
angelica-viorato <angelica.viorato@oracle.com>
2019-08-05 23:44:23 -0500

    Update Java FDK qr-code example to latest code

--- fdks/fdk-java/examples/qr-code/src/test/java/com/example/fn/QRGenTest.java
+++ fdks/fdk-java/examples/qr-code/src/test/java/com/example/fn/QRGenTest.java
@@ -1,4 +1,4 @@
-package com.fnproject.fn.examples;
+package com.example.fn;
 
 import com.fnproject.fn.testing.FnTestingRule;
 import com.google.zxing.BinaryBitmap;
@@ -23,32 +23,48 @@ public class QRGenTest {
     public FnTestingRule fn = FnTestingRule.createDefault();
 
     @Test
-    public void textHelloWorld() throws Exception {
+    public void textHelloWorld() {
         String content = "hello world";
+        String decodedImage = null;
         fn.givenEvent()
           .withHeader("Fn-Http-Request-Url", "http://www.example.com/qr?contents=hello+world&format=png")
           .withHeader("Fn-Http-Method","GET")
           .enqueue();
         fn.thenRun(QRGen.class, "create");
 
-        assertEquals(content, decode(fn.getOnlyResult().getBodyAsBytes()));
+        try {
+        	decodedImage = decode(fn.getOnlyResult().getBodyAsBytes());
+        } catch(Exception e) {
+        	System.err.println("Failed to decode image: " + e.getStackTrace());
+        }
+        
+        assertEquals(content, decodedImage);
     }
 
     @Test
-    public void phoneNumber() throws Exception {
+    public void phoneNumber() {
         String telephoneNumber = "tel:0-12345-67890";
+        String decodedImage = null;
         fn.givenEvent()
           .withHeader("Fn-Http-Request-Url", "http://www.example.com/qr?contents=tel:0-12345-67890")
           .withHeader("Fn-Http-Method","GET")
           .enqueue();
         fn.thenRun(QRGen.class, "create");
 
-        assertEquals(telephoneNumber, decode(fn.getOnlyResult().getBodyAsBytes()));
+        try {
+        	decodedImage = decode(fn.getOnlyResult().getBodyAsBytes());
+        } catch(Exception e) {
+        	System.err.println("Failed to decode image: " + e.getStackTrace());
+        }
+        
+        assertEquals(telephoneNumber, decodedImage);
+        
     }
 
     @Test
-    public void formatConfigurationIsUsedIfNoFormatIsProvided() throws Exception {
+    public void formatConfigurationIsUsedIfNoFormatIsProvided() {
         String contents = "hello world";
+        String decodedImage = null;
         fn.setConfig("FORMAT", "jpg");
         fn.givenEvent()
           .withHeader("Fn-Http-Request-Url", "http://www.example.com/qr?contents=hello+world")
@@ -56,15 +72,22 @@ public class QRGenTest {
           .enqueue();
         fn.thenRun(QRGen.class, "create");
 
-        assertEquals(contents, decode(fn.getOnlyResult().getBodyAsBytes()));
+        try {
+        	decodedImage = decode(fn.getOnlyResult().getBodyAsBytes());
+        } catch(Exception e) {
+        	System.err.println("Failed to decode image: " + e.getStackTrace());
+        }
+        
+        assertEquals(contents, decodedImage);
+        
     }
 
-    private String decode(byte[] imageBytes) throws IOException, NotFoundException, ChecksumException, FormatException {
+    private String decode(final byte[] imageBytes) throws IOException, NotFoundException, ChecksumException, FormatException {
         BinaryBitmap bitmap = readToBitmap(imageBytes);
         return new QRCodeReader().decode(bitmap).getText();
     }
 
-    private BinaryBitmap readToBitmap(byte[] imageBytes) throws IOException {
+    private BinaryBitmap readToBitmap(final byte[] imageBytes) throws IOException {
         BufferedImage inputImage = ImageIO.read(new ByteArrayInputStream(imageBytes));
         BufferedImageLuminanceSource luminanceSource = new BufferedImageLuminanceSource(inputImage);
         HybridBinarizer binarizer = new HybridBinarizer(luminanceSource);
